fd93aa6f5bfa0085392d696c1af6973a2b46579b
Anurag Maurya <anuragmaurya834@gmail.com>
2026-01-23 00:45:53 +0530

    Fix duplicate google verification route

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 from app import create_app
 from admin import admin_bp
 import os
-from flask import send_from_directory
 
 app = create_app()
 app.register_blueprint(admin_bp)
-
-# Google verification file route
-@app.route("/googlebe5a9285b24fdce8.html")
-def google_verify():
-    return send_from_directory("app/static", "googlebe5a9285b24fdce8.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
